704e497ae6ee79b03232df0c1ffbc06a44bddde6
dbakushkin <dany93@mail.ru>
2025-11-26 15:04:38 +0300

    fix

--- timer.js
+++ timer.js
@@ -1,6 +1,10 @@
 // timer
-const time = +process.argv[2];
+const hours = +process.argv[2].match(/\d+/) * 60 * 1000;
+const minutes = +process.argv[3].match(/\d+/) * 60 * 1000;
+const seconds = +process.argv[4].match(/\d+/) * 1000;
+const time = hours + minutes + seconds;
+console.log(time);
 
 setTimeout(() => {
   console.log("Таймер завершён");
-}, time * 1000);
+}, time);
